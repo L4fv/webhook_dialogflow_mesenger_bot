6ba228ef29b477ee69e761aee5d0e1297598eae2
luis flores <luanflov92@gmail.com>
2020-04-12 00:59:06 -0500

    UP
    Signed-off-by: luis flores <luanflov92@gmail.com>

--- api/controllers/template.js
+++ api/controllers/template.js
@@ -1,9 +1,24 @@
+const axios = require("axios")
 const template = async (req, res) => {
   try {
     //throw new Error('FUE UN ERROR CONTROLADO');
     console.log("BODY: ", req.body);
     const action = req.body.queryResult.action;
     console.log("ACTION: ", action);
+    console.log("user: ",req.body.originalDetectIntentRequest.payload.data)
+    var userId = req.body.originalDetectIntentRequest.payload.data.sender.id;
+    var tokenPage = "EAADjNAsHypIBANzPPj6CGX2c8OHZBG2tSVO7qRpnsFrekOhbWVUEEdsxmmasogszKOy5IlLzIx6R5pAOznlGUHOWPnK5ZBgStAlZAYzPxalmnRZA8DkeYJRfM90iZB9htPhF68uQEAna0go24KXOZCk3VVx75ZB2fpuFmVaNfAbXQZDZD";
+    const response = await axios({
+        url: `https://graph.facebook.com/v2.6/${userId}?fields=first_name,last_name,profile_pic&access_token=${tokenPage}`,
+        
+        method: "GET",
+        headers: {
+          "content-type": "application/json"
+          //Authorization: process.env.VUE_APP_AUTHORIZATION,
+        }        
+      });
+      console.log("datos: ",response.data)
+
     switch (action) {
       default:
         break;
