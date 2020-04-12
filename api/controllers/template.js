faeff37a459629430cf2dd13c8811b7b02836dcf
luis flores <luanflov92@gmail.com>
2020-04-12 00:32:49 -0500

    up
    Signed-off-by: luis flores <luanflov92@gmail.com>

--- api/controllers/template.js
+++ api/controllers/template.js
@@ -1,16 +1,18 @@
-
-
 const template = async (req, res) => {
-    try {
-        //throw new Error('FUE UN ERROR CONTROLADO');
-        console.log("BODY: ", req.body)
-        console.log("BODY2: ", req)
-    } catch (error) {
-        console.log("CATCH");
-        
+  try {
+    //throw new Error('FUE UN ERROR CONTROLADO');
+    console.log("BODY: ", req.body);
+    const action = req.body.queryResult.action;
+    console.log("ACTION: ", action);
+    switch (action) {
+      default:
+        break;
     }
-}
+  } catch (error) {
+    console.log("CATCH");
+  }
+};
 
 module.exports = {
-    template
-}
+  template,
+};
